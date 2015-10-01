c7b2c1b2d8fce51fea19d106592d50abc66474d6
gnikolaropoulos <g.nikolaropoulos@minetta.gr>
2015-10-01 16:02:35 +0300

    Fixing solution folder structure

---
+++ My Lib/System.SoftBytes/Validation/ISpecification.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="ISpecification.cs" company="BetKernel Ltd.">
+//     Copyright (c) 2010 BetKernel Ltd. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace BetKernel.Common.Validation
+{
+    public interface ISpecification<T>
+    {
+        /// <summary>
+        /// Determines whether [is satisfied by] [the specified t].
+        /// </summary>
+        /// <param name="t">The t.</param>
+        /// <returns>
+        /// 	<c>true</c> if [is satisfied by] [the specified t]; otherwise, <c>false</c>.
+        /// </returns>
+        Boolean IsSatisfiedBy(T t);
+    }
+}
